852fdee30a979a6cb9c649e89d067c8c8fb3ac29
Vildan <sekret.muzyafarov@yandex.ru>
2023-05-07 23:37:20 +0300

    feat: prettier config

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,10 +1,14 @@
-import { FC } from 'react';
+import { FC } from "react"
 
 const App: FC = () => {
-
-  return (
-    <div>Hello World!</div>
-  )
+	return (
+		<div
+			style={{}}
+			id=""
+		>
+			Hello World!
+		</div>
+	)
 }
 
 export default App
--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,13 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      src: "/src",
-      pages: "/src/pages",
-      components: "/src/components",
-      hooks: "/src/hooks",
-    }
-  }
+	plugins: [react()],
+	resolve: {
+		alias: {
+			src: "/src",
+			pages: "/src/pages",
+			components: "/src/components",
+			hooks: "/src/hooks",
+		}
+	}
 })
