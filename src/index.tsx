bc9d4884c3e4c2a070322e818ec89cc3ed06fd99
Vildan <sekret.muzyafarov@yandex.ru>
2023-05-10 20:19:31 +0300

    moved styles

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import App from "components/App"
 import React from "react"
 import ReactDOM from "react-dom/client"
 import { BrowserRouter } from "react-router-dom"
-import "./style.css"
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
