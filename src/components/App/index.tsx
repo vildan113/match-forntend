0955f084dd027146783621096f65d59ef48c708e
Vildan <sekret.muzyafarov@yandex.ru>
2023-05-29 12:26:51 +0300

    feat: betting-api

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,5 +1,7 @@
+import xBet from "@betting-api/1xbet"
 import { injectStores } from "@mobx-devtools/tools"
 import Header from "components/Header"
+import config from "config/index"
 import "normalize.css"
 import Home from "pages/Home"
 import { FC } from "react"
@@ -8,6 +10,10 @@ import store from "store/index"
 import "./style.css"
 
 injectStores({ store })
+
+xBet.registerApp({
+	secret_key: config.xBet.token
+})
 
 const App: FC = () => {
 	return (
